b2aa04333d55388255146ce2b60435296c73a921
shameer018 <50316368+shameer018@users.noreply.github.com>
2019-05-13 09:10:48 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -20,38 +20,6 @@ class App extends Component {
      }
 }
 
-class Stopwatch extends Component {
-  state = {
-    status: false,
-    runningTime: 0
-  };
-  handleClick = () => {
-    this.setState(state => {
-      if (state.status) {
-        clearInterval(this.timer);
-      } else {
-        const startTime = Date.now() - this.state.runningTime;
-        this.timer = setInterval(() => {
-          this.setState({ runningTime: Date.now() - startTime });
-        });
-      }
-      return { status: !state.status };
-    });
-  };
-  handleReset = () => {
-    clearInterval(this.timer);
-    this.setState({ runningTime: 0, status: false });
-  };
-  render() {
-    const { status, runningTime } = this.state;
-    return (
-      <div>
-        <p>{runningTime}ms</p>
-        <button onClick={this.handleClick}>{status ? 'Stop' : 'Start'}</button>
-        <button onClick={this.handleReset}>Reset</button>
-      </div>
-    );
-  }
-}
+
 
 export default App;
